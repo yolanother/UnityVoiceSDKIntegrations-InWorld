a04a66726ded00c02bc64739e3874238f44ba7d4
Yolan <yolanother@gmail.com>
2022-12-10 00:59:27 -0800

    Add speaker setup and OOD handler

---
+++ Scripts/Runtime/CharacterOutOfDomainHandler.cs
@@ -0,0 +1,52 @@
+using Inworld;
+using Meta.WitAi;
+using Meta.WitAi.CallbackHandlers;
+using Meta.WitAi.Json;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace VoiceSDK.Integrations.Inworld
+{
+    public class CharacterOutOfDomainHandler : WitResponseHandler
+    {
+        [SerializeField] private InworldInteraction _interaction;
+        [SerializeField] private UnityEvent<string> _onOutOfDomain = new UnityEvent<string>();
+
+        #if UNITY_EDITOR
+        [MenuItem("GameObject/Voice SDK/Character Out of Domain Handler", false, 10)]
+        private static void CreateComponent(MenuCommand menuCommand)
+        {
+            var go = new GameObject("Character Out of Domain Handler");
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            var handler = go.AddComponent<CharacterOutOfDomainHandler>();
+            handler._interaction = FindObjectOfType<InworldInteraction>();
+            if (Selection.activeGameObject)
+            {
+                handler.transform.parent = Selection.activeGameObject.transform;
+            }
+            Selection.activeObject = go;
+        }
+        #endif
+
+        protected override string OnValidateResponse(WitResponseNode response, bool isEarlyResponse)
+        {
+            if (response == null)
+            {
+                return "Response is null";
+            }
+            if (response["intents"].Count > 0)
+            {
+                return "Intents found";
+            }
+            return string.Empty;
+        }
+        protected override void OnResponseInvalid(WitResponseNode response, string error) {}
+        protected override void OnResponseSuccess(WitResponseNode response)
+        {
+            var transcription = response.GetTranscription();
+            _onOutOfDomain?.Invoke(transcription);
+            _interaction.SendText(transcription);
+        }
+    }
+}
---
+++ Scripts/Runtime/InworldInteractionSpeakerAdapter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Inworld;
+using Meta.WitAi;
+using Meta.WitAi.TTS.Integrations;
+using Meta.WitAi.TTS.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace VoiceSDK.Integrations.Inworld
+{
+    public class InworldInteractionSpeakerAdapter : MonoBehaviour
+    {
+        [SerializeField] private TTSSpeaker _speaker;
+        [SerializeField] private InworldInteraction _interaction;
+
+        private void OnEnable()
+        {
+            _interaction.InworldEvents.OnSpeak.AddListener(_speaker.SpeakQueued);
+        }
+
+        private void OnDisable()
+        {
+            _interaction.InworldEvents.OnSpeak.RemoveListener(_speaker.SpeakQueued);
+        }
+
+        [MenuItem("GameObject/Voice SDK/Inworld Character Speaker", false, 10)]
+        private static void CreateSpeaker()
+        {
+            var voiceService = FindObjectOfType<VoiceService>();
+            var runtimeConfig = (voiceService is Wit wit) ? wit.RuntimeConfiguration : null;
+            string[] witGuids;
+            var ttsWit = FindObjectOfType<TTSWit>();
+            if (!ttsWit)
+            {
+                witGuids = AssetDatabase.FindAssets("TTSWit");
+                if (witGuids.Length > 0)
+                {
+                    var witPath = AssetDatabase.GUIDToAssetPath(witGuids[0]);
+                    ttsWit = Instantiate(AssetDatabase.LoadAssetAtPath<TTSWit>(witPath));
+                    ttsWit.name = "TTSWit";
+                    ttsWit.RequestSettings.configuration = runtimeConfig?.witConfiguration;
+                }
+            }
+
+            TTSSpeaker speaker;
+            witGuids = AssetDatabase.FindAssets("TTSSpeaker");
+            if (witGuids.Length > 0)
+            {
+                var witPath = AssetDatabase.GUIDToAssetPath(witGuids[0]);
+                speaker = Instantiate(AssetDatabase.LoadAssetAtPath<TTSSpeaker>(witPath));
+                speaker.name = "TTSSpeaker";
+            }
+            else
+            {
+                var gameObject = new GameObject("Inworld Character Speaker");
+                speaker = gameObject.AddComponent<TTSSpeaker>();
+                var audioSource = speaker.gameObject.AddComponent<AudioSource>();
+                speaker.AudioSource = audioSource;
+            }
+
+            speaker.name = "Inworld Character Speaker";
+
+            var adapter = speaker.gameObject.AddComponent<InworldInteractionSpeakerAdapter>();
+            
+            adapter._speaker = speaker;
+            adapter._interaction = FindObjectOfType<InworldInteraction>();
+            if (adapter._interaction)
+            {
+                speaker.name = adapter._interaction.name + " Speaker";
+            }
+            if (Selection.activeGameObject)
+            {
+                speaker.transform.parent = Selection.activeGameObject.transform;
+            }
+        }
+    }
+}
